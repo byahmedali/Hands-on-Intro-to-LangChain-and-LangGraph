80f86a196fe8eecb5e27b1233ff0158dcc14e6e3
Ahmed Ali <byahmedali@gmail.com>
2025-08-13 00:21:41 +0500

    Updates to chatbot & notebook

--- chatbot.py
+++ chatbot.py
@@ -15,12 +15,10 @@ llm = ChatGroq(model="openai/gpt-oss-120b", temperature=0)
 # Define a new graph
 workflow = StateGraph(state_schema=MessagesState)
 
-
 # Define the function that calls the model
 def call_model(state: MessagesState):
     response = llm.invoke(state["messages"])
     return {"messages": response}
-
 
 # Define the (single) node in the graph
 workflow.add_edge(START, "model")
@@ -30,7 +28,6 @@ workflow.add_node("model", call_model)
 memory = MemorySaver()
 app = workflow.compile(checkpointer=memory)
 
-
 # Add a config thread
 config = {"configurable": {"thread_id": "abc123"}}
 
@@ -39,20 +36,17 @@ config = {"configurable": {"thread_id": "abc123"}}
 async def handle_message(message: cl.Message):
     # Convert user input to HumanMessage
     user_message = HumanMessage(content=message.content)
-    # Invoke the app with the correct message format
-    # response = app.invoke({"messages": [user_message]}, config)
-    # content = response["messages"][-1].content
-
     msg = cl.Message(content="")
     await msg.send()
-    
-    for chunk, metadata in app.stream({"messages": [user_message]},
-                                      config,
-                                      stream_mode="messages"):
+
+    # Streaming response from LLM
+    for chunk, metadata in app.stream(
+        {"messages": [user_message]}, config, stream_mode="messages"
+    ):
         if isinstance(chunk, AIMessage):  # Filter to just model responses
             # Stream the token
             await msg.stream_token(chunk.content)
             await asyncio.sleep(0.015)
-    
-    # Remove the typing cursor after streaming is complete
+
+    # Complete the streaming response
     await msg.send()
